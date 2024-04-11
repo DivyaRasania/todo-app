72e5046a5a7e797f64e1cd84f66aa634ec2a9ec2
DivyaRasania <rasaniadivya1@gmail.com>
2024-04-12 00:14:28 +0530

    made a skeleton for my project

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # todo-app
+
 A simple todo app created in React Native
----
+
+### Things I learnt from this project:
+
+1. Button
+2. StyleSheet
+3. Text
+4. TextInput
+5. View
