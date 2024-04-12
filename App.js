8fb023f31dfcf5ec95076296a9416f32b827ddbd
DivyaRasania <rasaniadivya1@gmail.com>
2024-04-12 20:54:00 +0530

    added some styles for inputContainer and textInput

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add goal" />
       </View>
       <View>
@@ -17,5 +17,23 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+    paddingHorizontal: 16,
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "hsl(0, 0%, 80%)",
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: "hsl(0, 0%, 80%)",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
   },
 });
